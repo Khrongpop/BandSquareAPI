3c61c2970db6e836515c582a75bb2d4cb86aba9e
Khrongpop Phonngam <mr.khrongpop@gmail.com>
2018-11-25 02:58:21 +0700

    tttt

--- main.go
+++ main.go
@@ -13,12 +13,12 @@ import (
 var db *gorm.DB
 var err error
 
-type User struct {
-	ID         int    `json:"id"`
-	Name       string `json:"name"`
-	Created_at string `json:created_at`
-	Updated_at string `json:updated_at`
-}
+// type User struct {
+// 	ID         int    `json:"id"`
+// 	Name       string `json:"name"`
+// 	Created_at string `json:created_at`
+// 	Updated_at string `json:updated_at`
+// }
 
 func main() {
 	viper.AutomaticEnv()
@@ -35,48 +35,48 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.POST("/create", create)
-	e.GET("/users", list)
-	e.GET("/users/:id", view)
-	e.PUT("/users/:id", update)
-	e.DELETE("/users/:id", delete)
+	// e.POST("/create", create)
+	// e.GET("/users", list)
+	// e.GET("/users/:id", view)
+	// e.PUT("/users/:id", update)
+	// e.DELETE("/users/:id", delete)
 	e.Logger.Fatal(e.Start(port))
 }
 
-func create(c echo.Context) error {
-	var user User
-	if err := c.Bind(&user); err != nil {
-		return err
-	}
-	// db.Create(&user)
-	return c.JSON(http.StatusOK, user)
-}
+// func create(c echo.Context) error {
+// 	var user User
+// 	if err := c.Bind(&user); err != nil {
+// 		return err
+// 	}
+// 	// db.Create(&user)
+// 	return c.JSON(http.StatusOK, user)
+// }
 
-func list(c echo.Context) error {
-	users := []User{}
-	// db.Find(&users)
-	return c.JSON(http.StatusOK, users)
-}
+// func list(c echo.Context) error {
+// 	users := []User{}
+// 	// db.Find(&users)
+// 	return c.JSON(http.StatusOK, users)
+// }
 
-func view(c echo.Context) error {
-	var user User
-	// db.First(&user, c.Param("id"))
-	return c.JSON(http.StatusOK, user)
-}
+// func view(c echo.Context) error {
+// 	var user User
+// 	// db.First(&user, c.Param("id"))
+// 	return c.JSON(http.StatusOK, user)
+// }
 
-func update(c echo.Context) error {
-	var user User
-	// db.First(&user, c.Param("id"))
-	// name := c.FormValue("name")
-	// db.Model(&user).Update("Name", name)
-	return c.JSON(http.StatusOK, user)
-}
+// func update(c echo.Context) error {
+// 	var user User
+// 	// db.First(&user, c.Param("id"))
+// 	// name := c.FormValue("name")
+// 	// db.Model(&user).Update("Name", name)
+// 	return c.JSON(http.StatusOK, user)
+// }
 
-func delete(c echo.Context) error {
-	var user User
-	// db.First(&user, c.Param("id"))
-	// db.Delete(&user)
-	return c.JSON(http.StatusOK, echo.Map{
-		"result": "success",
-	})
-}
+// func delete(c echo.Context) error {
+// 	var user User
+// 	// db.First(&user, c.Param("id"))
+// 	// db.Delete(&user)
+// 	return c.JSON(http.StatusOK, echo.Map{
+// 		"result": "success",
+// 	})
+// }
