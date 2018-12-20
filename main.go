f6ef1a5ca2c9869fdfa8191356f895d408dd5f9b
Khrongpop Phonngam <mr.khrongpop@gmail.com>
2018-12-21 01:13:45 +0700

    booking seed

--- main.go
+++ main.go
@@ -21,6 +21,12 @@ var err error
 
 func main() {
 
+	// input := "2017-08-31"
+	// layout := "2006-01-02"
+	// t, _ := time.Parse(layout, input)
+	// fmt.Println(t)                       // 2017-08-31 00:00:00 +0000 UTC
+	// fmt.Println(t.Format("02-Jan-2006")) // 31-Aug-2017
+
 	viper.AutomaticEnv()
 	port := ":" + viper.GetString("port")
 	// port := ":1323"
--- model/booking.go
+++ model/booking.go
@@ -5,17 +5,17 @@ import (
 )
 
 type Booking struct {
-	ID               uint      `gorm:"primary_key" json:"id"`
-	UserID           uint      `json:"user_id"`
-	BandID           *uint     `gorm:"nullable" json:"band_id"`
-	CategoryID       uint      `json:"category_id"`
-	TypeID           uint      `json:"type_id"`
-	Status           int       `gorm:"default:0" json:"status"`
-	Latitude         float64   `json:"latitude"`
-	Longitude        float64   `json:"longitude"`
-	Location         string    `json:"location"`
-	Date             time.Time `json:"date"`
-	Time             time.Time `json:"time"`
+	ID         uint      `gorm:"primary_key" json:"id"`
+	UserID     uint      `json:"user_id"`
+	BandID     *uint     `gorm:"nullable" json:"band_id"`
+	CategoryID uint      `json:"category_id"`
+	TypeID     uint      `json:"type_id"`
+	Status     int       `gorm:"default:0" json:"status"`
+	Latitude   float64   `json:"latitude"`
+	Longitude  float64   `json:"longitude"`
+	Location   string    `json:"location"`
+	DateTime   time.Time `json:"date_time"`
+	// Time             time.Time `json:"time"`
 	Duration         string    `json:"duration"`
 	Price            float64   `json:"price"`
 	Instrument       bool      `gorm:"default:false" json:"instrument"`
